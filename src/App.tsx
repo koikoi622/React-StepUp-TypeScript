1e54e7a9743e5ba6aabf6e3c114e7888d8561ef3
koikoi622 <104248045+koikoi622@users.noreply.github.com>
2022-05-03 22:25:18 +0900

    05032225

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
+import { Practice3 } from "./practices/Practice3";
+import { Practice4 } from "./practices/Practice4";
+import { Todo } from "./Todo";
+
 import "./styles.css";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
     </div>
   );
 }
